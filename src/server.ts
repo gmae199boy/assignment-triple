c75517545e0cdd6cdcdebe3f5ad64572a0df03cb
mark <34611716+gmae199boy@users.noreply.github.com>
2022-06-24 00:19:21 +0900

    DELETE: delete remarks

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,5 @@
 import app from "./app";
 
-// export default app.listen(process.env.SERVER_PORT, process.env.SERVER_ADDRESS, async function () {
-// 	console.log(process.env.SERVER_ADDRESS);
-// 	console.log(process.env.SERVER_PORT);
-// 	console.log(`server listening on`, app.server.address());
-// });
-
-// const start = async () => {
-// 	try {
-// 		await app.listen({ port: process.env.SERVER_PORT, host: process.env.SERVER_ADDRESS });
-// 	} catch (err) {
-// 		app.log.error(err);
-// 		process.exit(1);
-// 	}
-// };
-
-// const start = async () => {
-// 	try {
-// 		await app.listen({ port: 3000 });
-// 	} catch (err) {
-// 		app.log.error(err);
-// 		process.exit(1);
-// 	}
-// };
-
-// start();
 app.listen(
 	{ port: process.env.SERVER_PORT, host: process.env.SERVER_ADDRESS },
 	async function (err) {
